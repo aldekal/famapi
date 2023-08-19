0f5d13f9908def68c4bc892901c2fe70efe75ec9
aldekal <ahmed@aldekal.de>
2023-08-19 15:03:03 +0200

    mvn+springboot project init

---
+++ famgraph/src/main/java/www/famgraph/dev/FamgraphApplication.java
@@ -0,0 +1,13 @@
+package www.famgraph.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamgraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamgraphApplication.class, args);
+	}
+
+}
