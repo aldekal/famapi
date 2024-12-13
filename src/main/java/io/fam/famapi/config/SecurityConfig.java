80fc67771b3d1a8274e17a0cc62cabc73e6315bd
aldekal <ahmed@aldekal.de>
2024-12-13 09:28:48 +0100

    refactor(security): update security configuration to require authentication and use basic/form login

--- src/main/java/io/fam/famapi/config/SecurityConfig.java
+++ src/main/java/io/fam/famapi/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package io.fam.famapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,28 +15,28 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().permitAll() // Allow all requests without authentication
-                )
-                .csrf().disable(); // Disable CSRF protection if not needed
-        // @formatter:on
-        return http.build();
-    }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((authorize) -> authorize
+				.anyRequest().authenticated()
+			)
+			.httpBasic(Customizer.withDefaults())
+			.formLogin(Customizer.withDefaults());
 
-    // @formatter:off
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("ADMIN")
-                .build();
-        System.out.println("user: " + user.getAuthorities());
-        return new InMemoryUserDetailsManager(user);
-    }
-    // @formatter:on
+		return http.build();
+	}
+
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails userDetails = User.builder()
+			.username("user")
+			.password("{noop}password")
+			.roles("USER")
+			.build();
+
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+
 }
