967daef1158a645312626bc24052412ba705464d
aldekal <ahmed@aldekal.de>
2025-01-06 22:11:25 +0100

    refactor(user): remove UserController, User model, UserRepository, and UserService classes

--- src/main/java/io/fam/famapi/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package io.fam.famapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-import io.fam.famapi.model.User;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
-    
-}
